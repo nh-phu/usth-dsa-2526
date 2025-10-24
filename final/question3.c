2327b8127589465a56e6274d459a2247cfaf647d
nh-phu <nguyenhaiphu1662006@gmail.com>
2025-10-24 19:28:00 +0700

    Format

--- final/question3.c
+++ final/question3.c
@@ -1,9 +1,10 @@
 /*
- - Start from the root = 10, insert 10 into a stack. Now the stack not empty with 
-the node 10 in stack but it is also the only and the top element, pop the the 
-node 10, dispaly value 10, push left children 6 and right. Pop and display the top, 31
-Push 31 left and right node. Pop and display 17, push 17 left and right node. Pop
-and display 22, pop and display 14. Pop and display 110. Pop and display 6 then 1.
+ - Start from the root = 10, insert 10 into a stack. Now the stack not empty
+with the node 10 in stack but it is also the only and the top element, pop the
+the node 10, dispaly value 10, push left children 6 and right. Pop and display
+the top, 31 Push 31 left and right node. Pop and display 17, push 17 left and
+right node. Pop and display 22, pop and display 14. Pop and display 110. Pop and
+display 6 then 1.
 
 10 31 110 17 22 14 6  1
 
@@ -13,7 +14,7 @@ and display 22, pop and display 14. Pop and display 110. Pop and display 6 then 
  2. Display itself if it has no right node.
  3. Go to its left node
  The process:
- Start from 10, go to its right node, node 31 has  right node, go to its right node.
- Node 110 has none, display it self. 31 dont have right node anymore, display itself
- and go to the left. Continue and we have 110 31 22 17 14 10 6 1.
+ Start from 10, go to its right node, node 31 has  right node, go to its right
+node. Node 110 has none, display it self. 31 dont have right node anymore,
+display itself and go to the left. Continue and we have 110 31 22 17 14 10 6 1.
 */
